097b8119a72d4c3a5f729e4570f2d2078f6dd0aa
Amit Kapadia <amit@zooniverse.org>
2012-06-11 17:48:27 -0500

    WebGL demo working in Firefox.

--- public/demos/webgl-float-test.js
+++ public/demos/webgl-float-test.js
@@ -2,21 +2,27 @@ FITS = require("fits")
 
 $(document).ready(function() {
   var data, width, height, min, max, gl;
+  var program, fragmentShader;
   main();
 });
 
 function main() {
   
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', "http://0.0.0.0:9294/data/m101.fits", true);
+    // xhr.open('GET', "http://0.0.0.0:9294/data/m101.fits", true);
+    xhr.open('GET', "http://0.0.0.0:9294/data/CFHTLS_01_g_sci.fits", true);
+    // xhr.open('GET', "http://192.168.10.65:9294/data/m101.fits", true);
     xhr.responseType = 'arraybuffer';
     
     xhr.onload = function (e) {
       
       var fits = new FITS.File(xhr.response);
-      data = fits.hdus[0].data.getFrameWebGL()
+      data = fits.hdus[0].data.getFrameWebGL();
       width = fits.hdus[0].data.naxis[0];
       height = fits.hdus[0].data.naxis[1];
+      $("#canvas").attr("width", width);
+      $("#canvas").attr("height", height);
+      
       var extremes = fits.hdus[0].data.getExtremes();
       min = extremes[0];
       max = extremes[1];
@@ -24,26 +30,30 @@ function main() {
       setupUI(min, max);
       setupWebGL();
       
-      var stretch = $("#stretch").val()
+      var stretch = $("#stretch").val();
       render(width, height, stretch, min, max, data);
-    };
+    }
     
     xhr.send();
 }
 
 function setupUI(min, max) {
-  $( "#slider-range" ).slider({
+
+  $("#slider-range").slider({
     range: true,
     min: min,
     max: max,
     values: [min, max],
     slide: function( event, ui ) {
-      console.log(event);
-      var stretch = parseFloat($("#stretch").val());
+      var stretch = $("#stretch").val();
       var extremes = ui.values;
-      render(width, height, stretch, extremes[0], extremes[1], data);
+      var extremesLocation = gl.getUniformLocation(program, "u_extremes");
+      gl.uniform2f(extremesLocation, extremes[0], extremes[1]);
+      gl.drawArrays(gl.TRIANGLES, 0, 6);
     }
   });
+  
+  $(".ui-slider-horizontal").css("width", width);
   
   $("#stretch").change(function() {
     var stretch = $("#stretch").val();
@@ -66,7 +76,6 @@ function setupWebGL() {
       alert("no OES_texture_float");
       return;
   }
-  
 }
 
 function render(width, height, stretch, min, max, data) {
@@ -82,7 +91,7 @@ function render(width, height, stretch, min, max, data) {
   
   var extremesLocation = gl.getUniformLocation(program, "u_extremes");
   gl.uniform2f(extremesLocation, min, max);
-
+  
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
@@ -90,10 +99,10 @@ function render(width, height, stretch, min, max, data) {
        -1,  1, 1, -1,  1, 1]), gl.STATIC_DRAW);
   gl.enableVertexAttribArray(positionLocation);
   gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-
+  
   var tex = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, tex);
-
+  
   gl.texImage2D(
       gl.TEXTURE_2D, 0, 
       gl.LUMINANCE, width, height, 0,
@@ -102,7 +111,10 @@ function render(width, height, stretch, min, max, data) {
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
+  
   gl.drawArrays(gl.TRIANGLES, 0, 6);
   console.log(gl.getError());
+  // var dataTransfer = new Uint8Array(4 * width);
+  // gl.readPixels(0, 0, width, 1, gl.RGBA, gl.UNSIGNED_BYTE, dataTransfer);
+  // console.log(dataTransfer);
 }
